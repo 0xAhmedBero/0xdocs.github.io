405757421896fa4a503d9150c2d6c1e440675e98
ahmed bero <ahmedbero543@gmail.com>
2024-12-28 20:55:25 +0100

    change editUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -31,7 +31,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/0xAhmedBero/0xdocs.github.io',
+            'https://github.com/0xAhmedBero/0xdocs.github.io/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
